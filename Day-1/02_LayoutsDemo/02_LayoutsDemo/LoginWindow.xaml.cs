5276d743b1dbd8d7e6c2a44ee8c3efaab09e8e7e
Magesh <tkmagesh77@gmail.com>
2013-06-24 14:28:40 +0530

    Initial

---
+++ Day-1/02_LayoutsDemo/02_LayoutsDemo/LoginWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_LayoutsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            string status = string.Empty;
+            if (TxtUsername.Text == PwdPassword.Password)
+            {
+                status = "Login successful";
+                TbStatus.Foreground = new SolidColorBrush(new Color(){R=0, G = 255, B=0, A=255});
+            }
+            else
+            {
+                status = "Invalid Credentials";
+                TbStatus.Foreground = new SolidColorBrush(new Color() { R = 255, G = 0, B = 0, A = 255 });
+            }
+            TbStatus.Text = status;
+        }
+    }
+}
