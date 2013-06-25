82812a0395684197e70ea8c508f55e7062e9ee46
Magesh <tkmagesh77@gmail.com>
2013-06-25 11:11:23 +0530

    MultiBinding Demo

---
+++ Day-2/01_MultiBindingDemo/01_MultiBindingDemo/SliderValuesToBrushConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _01_MultiBindingDemo
+{
+    public class SliderValuesToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var red = System.Convert.ToByte(values[0]);
+            var green = System.Convert.ToByte(values[1]);
+            var blue = System.Convert.ToByte(values[2]);
+            var alpha = System.Convert.ToByte(values[3]);
+            var newColor = new Color()
+            {
+                R = red,
+                G = green,
+                B = blue,
+                A = alpha
+            };
+            if (targetType.Name == "Brush")
+            {
+                
+               return new SolidColorBrush(newColor);
+            }
+            return newColor.ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
