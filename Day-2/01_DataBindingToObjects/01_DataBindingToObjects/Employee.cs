d19685c128a5f10ecd18f1fb7f419c7e621c7c75
Magesh <tkmagesh77@gmail.com>
2013-06-25 16:48:11 +0530

    CollectionBinding & Command

--- Day-2/01_DataBindingToObjects/01_DataBindingToObjects/Employee.cs
+++ Day-2/01_DataBindingToObjects/01_DataBindingToObjects/Employee.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace _01_DataBindingToObjects
 {
+   
+
     public class Employee : INotifyPropertyChanged
     {
         private string _firstName;
+        private string _lastName;
+
         public string FirstName
         {
             get { return _firstName; }
@@ -16,7 +21,7 @@ namespace _01_DataBindingToObjects
             {
                 _firstName = value;
                 TriggerPropertyChanged("FirstName");
-                TriggerPropertyChanged("FullName");
+                TriggerPropertyChanged("CanFullNameBeGenerated");
             }
         }
 
@@ -26,16 +31,142 @@ namespace _01_DataBindingToObjects
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public string LastName { get; set; }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                TriggerPropertyChanged("LastName");
+                TriggerPropertyChanged("CanFullNameBeGenerated");
+            }
+        }
+
+        private string _fullName;
+        private string _greetMessage;
+        private int _id;
+
+        public Employee()
+        {
+            
+        }
+
         public string FullName
         {
-            get { return FirstName + " " + LastName; }
+            get { return _fullName; }
+            set { 
+                _fullName = value;
+                TriggerPropertyChanged("FullName");
+            }
         }
+
+        
+
+       
+        public bool CanFullNameBeGenerated
+        {
+            get { return !(string.Empty.Equals(FirstName) || string.Empty.Equals(LastName)); }
+         
+        }
+
         public override string ToString()
         {
             return string.Format("FirstName = {0}, LastName={1}, FullName={2}", FirstName, LastName, FullName);
         }
 
+        
+        
+
+        public string GreetMessage
+        {
+            get { return _greetMessage; }
+            set
+            {
+                _greetMessage = value;
+                TriggerPropertyChanged("GreetMessage");
+            }
+        }
+
+        
+        public void GenerateFullName(object parameter)
+        {
+            FullName = FirstName + " " + LastName;
+        }
+        /*private class GenerateFullNameCommandClass : ICommand
+        {
+            private readonly Employee _employee;
+
+            public GenerateFullNameCommandClass(Employee employee)
+            {
+                _employee = employee;
+            }
+
+            public void Execute(object parameter)
+            {
+                _employee.GenerateFullName();
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+        }*/
+        public ICommand GenerateFullNameCommand
+        {
+            get { return new MyCommand(GenerateFullName, o => CanFullNameBeGenerated); }
+        }
+
+        public ICommand GreetEmployeeCommand
+        {
+            get { return new MyCommand(GreetEmployee, o => true); }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    TriggerPropertyChanged("Id");
+                }
+            }
+        }
+
+        /* public class GreetEmployeeCommandClass : ICommand
+        {
+            private readonly Employee _employee;
+
+            public GreetEmployeeCommandClass(Employee employee)
+            {
+                _employee = employee;
+            }
+
+            public void Execute(object parameter)
+            {
+                
+                _employee.GreetEmployee();
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+        }*/
+
+        private void GreetEmployee(object parameter)
+        {
+            GreetMessage = "Hi " + (DateTime.Now.Hour < 12 ? "Good Morning " : "Good Afternoon ") + FirstName;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+        
     }
 }
---
+++ Day-2/01_DataBindingToObjects/01_DataBindingToObjects/ListBinding.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _01_DataBindingToObjects
+{
+    /// <summary>
+    /// Interaction logic for ListBinding.xaml
+    /// </summary>
+    public partial class ListBinding : Window
+    {
+        private ObservableCollection<Employee> employees;
+
+        public ListBinding()
+        {
+            InitializeComponent();
+            employees = new ObservableCollection<Employee>()
+                {
+                    new Employee() {FirstName = "Magesh", LastName = "K", Id = 101},
+                    new Employee() {FirstName = "Suresh", LastName = "K", Id = 102},
+                    new Employee() {FirstName = "Ganesh", LastName = "E", Id = 103},
+                    new Employee() {FirstName = "Rajesh", LastName = "P", Id = 104}
+                };
+            
+            ListEmployees.ItemsSource = employees;
+        }
+
+        private void BtnAddEmployee_OnClick(object sender, RoutedEventArgs e)
+        {
+            employees.Add(new Employee()
+                {
+                    FirstName = "Yuva", LastName = "K", Id = 105
+                });
+            MessageBox.Show(employees.Count.ToString());
+        }
+    }
+}
